4bfb97b641f1ed59e668b39283adbe3019420f47
Pedro Tomaz <tomaz.pedro321@gmai.com>
2025-11-20 19:10:50 -0300

    Adicionado o Front-end e resolvido alguns bugs

--- SRC/Models/emprestimosModel.js
+++ SRC/Models/emprestimosModel.js
@@ -1,27 +1,27 @@
 const connection = require ('../Config/db');
 const emprestimosModel = {
     buscarTodos: (callback) => {
-        const sql = 'SELECT * FROM emprestimos';
+        const sql = 'SELECT * FROM Emprestimos';
         connection.query(sql, callback);
     },
 
     buscarPorId: (id, callback) => {
-        const sql = 'SELECT * FROM emprestimos WHERE id = ?';
+        const sql = 'SELECT * FROM Emprestimos WHERE id = ?';
         connection.query(sql, [id], callback);
     },
 
     criarEmprestimo: (dados, callback) => {
-            const sql = "INSERT INTO emprestimos (id_livro, nome_pessoa, data_emprestimo) VALUES (?, ?, ?)";
+            const sql = "INSERT INTO Emprestimos (id_livro, nome_pessoa, data_emprestimo) VALUES (?, ?, ?)";
             connection.query(sql, [dados.id_livro, dados.nome_pessoa, dados.data_emprestimo], callback);
 },
 
     atualizarEmprestimo: (id, dados, callback) => {
-        const sql = "UPDATE emprestimos SET id_livro = ?, nome_pessoa = ?, data_emprestimo = ? WHERE id = ?"
+        const sql = "UPDATE Emprestimos SET id_livro = ?, nome_pessoa = ?, data_emprestimo = ? WHERE id = ?"
         connection.query(sql, [dados.id_livro, dados.nome_pessoa, dados.data_emprestimo, id], callback)
     },
 
     deletarEmprestimo: (id, callback) => {
-        const sql ="DELETE FROM emprestimos WHERE id = ?";
+        const sql ="DELETE FROM Emprestimos WHERE id = ?";
         connection.query(sql, [id], callback);
     }
 };
--- SRC/Models/usuarioModel.js
+++ SRC/Models/usuarioModel.js
@@ -2,25 +2,25 @@ const connection = require ('../Config/db');
 
 const usuarioModel = {
     buscarTodos: (callback) => {
-        const sql = 'SELECT * FROM usuario';
+        const sql = 'SELECT * FROM Usuarios';
         connection.query(sql, callback);
     },
 
     buscarPorId: (id, callback) => {
-        const sql = 'SELECT * FROM usuario WHERE id = ?';
+        const sql = 'SELECT * FROM Usuarios WHERE id = ?';
         connection.query(sql, [id], callback);
     },
 
     criarUsuario: (dados, callback) => {
-            const sql = "INSERT INTO usuario (nome, matricula, tipo) VALUES (?, ?, ?)";
+            const sql = "INSERT INTO Usuarios (nome, matricula, tipo) VALUES (?, ?, ?)";
             connection.query(sql, [dados.nome, dados.matricula, dados.tipo], callback);
     },
     atualizarUsuario : (id, dados, callback) => {
-        const sql = "UPDATE usuario SET nome = ?, matricula = ?, tipo = ? WHERE id = ? ";
+        const sql = "UPDATE Usuarios SET nome = ?, matricula = ?, tipo = ? WHERE id = ? ";
         connection.query(sql, [dados.nome, dados.matricula, dados.tipo, id], callback);
     },
     deletarUsuario : (id, callback) => {
-        const sql = "DELETE FROM usuario WHERE id = ?";
+        const sql = "DELETE FROM Usuarios WHERE id = ?";
         connection.query(sql, [id], callback);
     }
 };
