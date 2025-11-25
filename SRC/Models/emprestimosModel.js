05cf0f36f51997051b4c0e5b3199116eca020a68
Ericfernn <ericferne03@gmail.com>
2025-11-25 17:28:09 -0300

    Update emprestimosModel.js
    Comentários

--- SRC/Models/emprestimosModel.js
+++ SRC/Models/emprestimosModel.js
@@ -1,28 +1,28 @@
-const connection = require ('../Config/db');
+const connection = require ('../Config/db'); 
 const emprestimosModel = {
-    buscarTodos: (callback) => {
-        const sql = 'SELECT * FROM Emprestimos';
-        connection.query(sql, callback);
+    buscarTodos: (callback) => { // Método para buscar todos os empréstimos
+        const sql = 'SELECT * FROM Emprestimos';// Consulta SQL que retorna todos os registros
+        connection.query(sql, callback);// Executa a query e usa o callback para retornar o resultado
     },
 
-    buscarPorId: (id, callback) => {
-        const sql = 'SELECT * FROM Emprestimos WHERE id = ?';
-        connection.query(sql, [id], callback);
+    buscarPorId: (id, callback) => {// Método para buscar empréstimo específico pelo ID
+        const sql = 'SELECT * FROM Emprestimos WHERE id = ?';// Query com parâmetro para buscar pelo ID
+        connection.query(sql, [id], callback);// Executa a query, passando o ID como parâmetro
     },
 
-    adicionarEmprestimo: (dados, callback) => {
-            const sql = "INSERT INTO Emprestimos (idLivro, idUsuario, dataEmprestimo, dataDevolucao) VALUES (?, ?, ?, ?)";
+    adicionarEmprestimo: (dados, callback) => {// Método para adicionar um novo empréstimo
+            const sql = "INSERT INTO Emprestimos (idLivro, idUsuario, dataEmprestimo, dataDevolucao) VALUES (?, ?, ?, ?)";// Query de inserção
             connection.query(sql, [ dados.idLivro, dados.idUsuario,dados.dataEmprestimo,dados.dataDevolucao], callback);
 },
 
     atualizarEmprestimo: (id, dados, callback) => {
-         const sql = "UPDATE Emprestimos SET idLivro = ?, idUsuario = ?, dataEmprestimo = ?, dataDevolucao = ? WHERE id = ?";
+         const sql = "UPDATE Emprestimos SET idLivro = ?, idUsuario = ?, dataEmprestimo = ?, dataDevolucao = ? WHERE id = ?"; // Valores que serão inseridos
         connection.query(sql, [dados.idLivro, dados.idUsuario, dados.dataEmprestimo, dados.dataDevolucao, id], callback);
 },
 
-    deletarEmprestimo: (id, callback) => {
-        const sql ="DELETE FROM Emprestimos WHERE id = ?";
-        connection.query(sql, [id], callback);
+    deletarEmprestimo: (id, callback) => { // Método para atualizar empréstimo existente
+        const sql ="DELETE FROM Emprestimos WHERE id = ?";// Query de update
+        connection.query(sql, [id], callback);// Novos valores + ID
     }
 };
 
